4af70b81c087d1572c5e295962115aa22becb168
Fangfang Xia <fangfang.xia@gmail.com>
2017-04-18 16:33:23 -0400

    add autoencoder codes

---
+++ Autoencoder-NIH/README.md
@@ -0,0 +1,10 @@
+ECP-CANDLE Workshop at NIH
+April 18, 2017
+
+Fangfang Xia
+
+* Motivating example: learning addition with sequence to sequence model
+
+* Keras MNIST MLP
+* Keras Autoencoder
+* Keras Denoising Autoencoder
---
+++ Autoencoder-NIH/mnist_denoising_autoencoder.py
@@ -0,0 +1,100 @@
+
+from __future__ import print_function
+
+import numpy as np
+
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Input, Dense, Dropout
+from keras.optimizers import RMSprop
+
+
+batch_size = 128
+num_classes = 10
+# epochs = 20
+epochs = 5
+
+# the data, shuffled and split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = x_train.reshape(60000, 784)
+x_test = x_test.reshape(10000, 784)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print('x_test shape:', x_test.shape)
+
+noise_factor = 0.5
+x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape)
+x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape)
+
+x_train_noisy = np.clip(x_train_noisy, 0., 1.)
+x_test_noisy = np.clip(x_test_noisy, 0., 1.)
+
+
+# %matplotlib inline
+import matplotlib
+import matplotlib.pyplot as plt
+
+from keras.layers import Input, Dense
+from keras.models import Model
+
+# this is the size of our encoded representations
+encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
+
+# this is our input placeholder
+input_img = Input(shape=(784,))
+# "encoded" is the encoded representation of the input
+encoded = Dense(encoding_dim, activation='relu')(input_img)
+# "decoded" is the lossy reconstruction of the input
+decoded = Dense(784, activation='sigmoid')(encoded)
+
+# this model maps an input to its reconstruction
+autoencoder = Model(input=input_img, output=decoded)
+print("autoencoder model created")
+
+# this model maps an input to its encoded representation
+encoder = Model(input=input_img, output=encoded)
+
+# create a placeholder for an encoded (32-dimensional) input
+encoded_input = Input(shape=(encoding_dim,))
+# retrieve the last layer of the autoencoder model
+decoder_layer = autoencoder.layers[-1]
+# create the decoder model
+decoder = Model(input=encoded_input, output=decoder_layer(encoded_input))
+
+autoencoder.summary()
+
+autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+
+
+history = autoencoder.fit(x_train_noisy, x_train,
+                          batch_size=batch_size, epochs=epochs,
+                          verbose=1, validation_data=(x_test_noisy, x_test))
+
+# encode and decode some digits
+# note that we take them from the *test* set
+encoded_imgs = encoder.predict(x_test)
+decoded_imgs = decoder.predict(encoded_imgs)
+
+n = 10  # how many digits we will display
+fig = plt.figure(figsize=(20, 4))
+for i in range(10):
+    # display original
+    ax = plt.subplot(2, n, i + 1)
+    plt.imshow(x_test_noisy[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display reconstruction
+    ax = plt.subplot(2, n, i + 1 + n)
+    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+# plt.show()
+fig.savefig("denoising_autoencoded_digits.png")
